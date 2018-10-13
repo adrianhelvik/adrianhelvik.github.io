f8416d0429d77458904669fc7234704503802910
Adrian Helvik <adrian.helvik@specifique.no>
2018-10-14 00:38:18 +0200

    Added proposal-move-events

---
+++ proposal-move-events/src/snapshot.js
@@ -0,0 +1,33 @@
+import cloneNode from './cloneNode.js'
+
+export function polyfill() {
+  document.createSnapshot = createSnapshot
+}
+
+function createSnapshot(element) {
+  return new Snapshot(element)
+}
+
+class Snapshot {
+  constructor(element) {
+    this.element = element
+    this.clone = cloneNode(this.element)
+    this.clone.style.position = 'fixed'
+    this.clone.style.left = 0
+    this.clone.style.top = 0
+    this.clone.style.pointerEvents = 'none'
+  }
+
+  place({ x, y }) {
+    this.move({ x, y })
+    document.body.appendChild(this.clone)
+  }
+
+  move({ x, y }) {
+    this.clone.style.transform = `translateX(${x}px) translateY(${y}px)`
+  }
+
+  remove() {
+    this.clone.remove()
+  }
+}
